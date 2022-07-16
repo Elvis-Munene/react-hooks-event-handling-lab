f099913eca9da5751c9130dc716faec557eb6112
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-16 20:52:06 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+import onfocus from 'react'
+import onblur from 'react'
+
+function focusBlur() {
+  console.log('Hey! Eyes on me!')
+}
+
+function focusOnfocus() {
+  console.log('Good!')
+}
+
+function EyesOnMe() {
+  return (
+    <button onFocus={focusOnfocus} onBlur={focusBlur}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+import React from 'react'
+
+function obtainPassword() {
+  console.log('Entering password...')
+}
+
+function Keypad() {
+  return (
+    <input type="password" onChange={obtainPassword}/>
+  )
+}
+
+export default Keypad
+
